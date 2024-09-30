e3e7488af09eb797426e777122e4836cf4bd12f8
JaeYoun Kim <rlawodusx@naver.com>
2024-10-01 00:10:51 +0900

    Feat: 텍스트 메쉬 프로 UGUI

---
+++ Assets/Scripts/Quiz.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Quiz : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] QuestionSO question;
+
+    
+    void Start()
+    {
+        questionText.text = question.GetQuestion();
+    }
+}
