fb3660d36da9ef28bdcc04722a6a9cd8071ac033
JaeYoun Kim <rlawodusx@naver.com>
2024-09-30 22:21:39 +0900

    Feat: 배열

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -12,10 +12,27 @@ public class QuestionSO : ScriptableObject
     // TextArea: 인스펙터 안의 텍스트 상자의 크기를 지정하고 조정할 수 있음 
     [TextArea(2, 6)]
     [SerializeField] string question = "Enter new question text here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
+
+
 
     public string GetQuestion()
     {
         return question;
     }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
+
+
+
 
 }
