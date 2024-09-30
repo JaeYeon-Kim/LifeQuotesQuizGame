01b62ffa42f41282a5c9fb39edf35378d7f00f26
JaeYoun Kim <rlawodusx@naver.com>
2024-10-01 01:06:36 +0900

    Feat: 답안 추가

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -7,10 +7,21 @@ public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;    // 버튼에 대한 배열 
 
-    
+
     void Start()
     {
         questionText.text = question.GetQuestion();
+
+
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+
+            buttonText.text = question.GetAnswer(i);
+        }
+
     }
 }
