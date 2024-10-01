4b342aedae253f5946c20edb077ef73c43098e02
JaeYoun Kim <rlawodusx@naver.com>
2024-10-02 02:39:53 +0900

    Feat: 단순 타이머

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
@@ -9,11 +11,57 @@ public class Quiz : MonoBehaviour
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;    // 버튼에 대한 배열 
 
+    // 올바른 정답의 인덱스 참조 변수 
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
+
+
 
     void Start()
     {
-        questionText.text = question.GetQuestion();
+        GetNextQuestion();
+        //DisplayQuestion();
+    }
 
+    // 버튼을 눌렀을때 호출할 메소드 (정답 체크 시)
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            // 정답일때 
+            questionText.text = "Correct!";
+            // 선택한 버튼의 이미지를 가져오고 정답 임을 표시하는 강조 이미지로 변경해줌 
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        // 정답이 아닐 경우 
+        else
+        {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();  // 정답 인덱스 가져오기 
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = "Sorry, the correct answer was;\n" + correctAnswer;
+
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+
+        SetButtonState(false);
+    }
+
+    void GetNextQuestion()
+    {
+        SetButtonState(true);
+        SetDefaultButtonSprites();
+        DisplayQuestion();
+    }
+
+
+    private void DisplayQuestion()
+    {
+        questionText.text = question.GetQuestion();
 
         for (int i = 0; i < answerButtons.Length; i++)
         {
@@ -22,6 +70,23 @@ public class Quiz : MonoBehaviour
 
             buttonText.text = question.GetAnswer(i);
         }
+    }
 
+    // 버튼의 상태를 관리하는 메소드
+    void SetButtonState(bool state)
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Button button = answerButtons[i].GetComponent<Button>();
+            button.interactable = state;
+        }
+    }
+    private void SetDefaultButtonSprites()
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Image buttonImage = answerButtons[i].GetComponent<Image>();
+            buttonImage.sprite = defaultAnswerSprite;
+        }
     }
 }
